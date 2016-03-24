bb177f2d6a5c05c80cfbeebad461c46de4e1ceda
samuraisam417 <samuel.mineer@gmail.com>
2016-03-24 12:12:17 -0600

    First commit for Hiking Trail Explorer by Jasmyn Harper and Samuel Mineer

---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+import sys
+from setuptools import setup, find_packages
+from tethys_apps.app_installation import custom_develop_command, custom_install_command
+
+### Apps Definition ###
+app_package = 'hiking_app_js'
+release_package = 'tethysapp-' + app_package
+app_class = 'hiking_app_js.app:HikingTrailExplorer'
+app_package_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'tethysapp', app_package)
+
+### Python Dependencies ###
+dependencies = []
+
+setup(
+    name=release_package,
+    version='0.1',
+    description='',
+    long_description='',
+    keywords='',
+    author='Jasmyn Harper and Samuel Mineer',
+    author_email='',
+    url='',
+    license='MIT License',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=['tethysapp', 'tethysapp.' + app_package],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=dependencies,
+    cmdclass={
+        'install': custom_install_command(app_package, app_package_dir, dependencies),
+        'develop': custom_develop_command(app_package, app_package_dir, dependencies)
+    }
+)
---
+++ tethysapp/hiking_app_js/app.py
@@ -0,0 +1,31 @@
+from tethys_sdk.base import TethysAppBase, url_map_maker
+
+
+class HikingTrailExplorer(TethysAppBase):
+    """
+    Tethys app class for Hiking Trail Explorer.
+    """
+
+    name = 'Hiking Trail Explorer'
+    index = 'hiking_app_js:home'
+    icon = 'hiking_app_js/images/icon.gif'
+    package = 'hiking_app_js'
+    root_url = 'hiking-app-js'
+    color = '#34495e'
+    description = 'Place a brief description of your app here.'
+    enable_feedback = False
+    feedback_emails = []
+
+        
+    def url_maps(self):
+        """
+        Add controllers
+        """
+        UrlMap = url_map_maker(self.root_url)
+
+        url_maps = (UrlMap(name='home',
+                           url='hiking-app-js',
+                           controller='hiking_app_js.controllers.home'),
+        )
+
+        return url_maps
